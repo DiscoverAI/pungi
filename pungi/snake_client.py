49dc4028bc448d93448bf9e450f18c01f9296733
Falco Winkler <falco.winkler@esailors.de>
2019-03-23 21:09:26 +0000

    Reimplement client properly with tests

--- pungi/snake_client.py
+++ pungi/snake_client.py
@@ -1,5 +1,20 @@
 import requests
 
-def register_game(param, param1, param2, param3):
-    requests.post(param + "/games", data={'height': 1, 'width': 1, 'snakeLength': 1})
-    return "foo"
+
+class SnakeClient:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def register_game(self, board_width, board_height, snake_length):
+        return requests.post(self.base_url + '/games',
+                             json={'height': board_height,
+                                   'width': board_width,
+                                   'snakeLength': snake_length},
+                             headers={'Accept': 'application/json'}).json()['gameId']
+
+    def make_move(self, direction, game_id):
+        return requests.put(self.base_url + '/games/'
+                            + game_id
+                            + '/tokens/snake/direction',
+                            json={'direction': direction},
+                            headers={'Accept': 'application/json'}).json()
--- tests/test_snake_client.py
+++ tests/test_snake_client.py
@@ -1,10 +1,30 @@
-from pungi import snake_client
+from unittest.mock import patch
+
+from pungi.snake_client import SnakeClient
+from requests.models import Response
+
+mock_register_response = Response()
+mock_register_response.json = lambda: {"gameId": "foobar"}
+
+mock_move_response = Response()
+mock_move_response.json = lambda: {"game-info-key": "game-info-value"}
 
 
-def test_register_game(mocker):
-    requests_mock = mocker.patch('requests.post')
-    requests_mock.returnValue = 'foo'
-    game_id = snake_client.register_game('foobar', 1, 1, 1)
+@patch('requests.post', return_value=mock_register_response)
+def test_register_game(post_mock):
+    client = SnakeClient("http://example.com")
+    game_id = client.register_game(board_width=1, board_height=1, snake_length=1)
+    assert 'foobar' == game_id
+    post_mock.assert_called_with('http://example.com/games',
+                                 headers={'Accept': 'application/json'},
+                                 json={'height': 1, 'snakeLength': 1, 'width': 1})
 
-    assert 'foo' == game_id
-    requests_mock.assert_called_with('foobar/games', data={'height': 1, 'snakeLength': 1, 'width': 1})
+
+@patch('requests.put', return_value=mock_move_response)
+def test_register_game(put_mock):
+    client = SnakeClient("http://example.com")
+    game_info = client.make_move(direction="up", game_id="foobar")
+    assert {"game-info-key": "game-info-value"} == game_info
+    put_mock.assert_called_with('http://example.com/games/foobar/tokens/snake/direction',
+                                headers={'Accept': 'application/json'},
+                                json={"direction": "up"})
