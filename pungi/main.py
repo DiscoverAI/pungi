75f128d158b9f88d1a27e51ab5f80c25ef0373ce
Falco Winkler <falcomail95@yahoo.de>
2019-05-07 07:57:14 +0000

    Added saving model to main method

--- pungi/main.py
+++ pungi/main.py
@@ -1,7 +1,10 @@
 import pungi.trainer as trainer
 from pungi.ml_agent import play_in_spectator_mode
+import pungi.persistence as persistence
+import time
 
 if __name__ == '__main__':
     q_table = trainer.train()
+    persistence.save(q_table, "./out/model-" + str(int(time.time())) + ".pkl")
     for i in range(5):  # play some example games
         play_in_spectator_mode(q_table)
