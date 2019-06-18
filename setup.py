f63aadc5ce7f4e3a68e3a58b29e4dde7431d980b
Daniel Schruhl <danielschruhl@gmail.com>
2019-06-18 09:52:40 +0200

    make project installable

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pungi",
+    version="0.1.0",
+    author="DiscoverAI",
+    description="Q-learning agent that is learning how to play snake.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiscoverAI/pungi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
