ba230db1b739db6135cdb1a39e37a43bbc2c1e04
Falco Winkler <falcomail95@yahoo.de>
2019-07-29 16:18:29 +0000

    configure maxlength for replay memory

--- pungi/agents/dql/dql_agent.py
+++ pungi/agents/dql/dql_agent.py
@@ -4,11 +4,16 @@ from collections import deque
 
 
 class DQLAgent(Agent):
+    def __init__(self, configuration=None):
+        self.configuration = configuration
+        self.replay_memory = None
+        self.init_replay_memory(configuration['replay_memory_limit'])
+
     def next_action(self, state, episode_number):
         pass
 
-    def __init__(self):
-        self.replay_memory = deque()
+    def init_replay_memory(self, limit):
+        self.replay_memory = deque(maxlen=limit)
 
     def update(self, state, action, nextstate, reward):
         self.replay_memory.append((state, action, nextstate, reward))
--- tests/agents/dql/test_dql_agent.py
+++ tests/agents/dql/test_dql_agent.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch, ANY
 import pungi.agents.dql.dql_agent as dql_agent
 import numpy as np
+import collections
 
 
 def test_agent_experience_replay_single_step():
@@ -34,3 +35,10 @@ def test_agent_experience_replay_four_steps(sample_mock):
     agent.update(*memory_entry2)
     assert agent.sample_memory(64) == [memory_entry1, memory_entry2]
     sample_mock.assert_called_with(ANY, 2)
+
+
+def test_should_create_agent_with_limitied_replay_memory(mocker):
+    init_replay_memory_mock = mocker.patch('pungi.agents.dql.dql_agent.DQLAgent.init_replay_memory')
+    agent = dql_agent.DQLAgent({'replay_memory_limit': 42})
+    assert agent is not None
+    init_replay_memory_mock.assert_called_with(42)
