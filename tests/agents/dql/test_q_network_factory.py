f0a974bdf425c2a7938a08c3eefa7e2c2f2fc5b5
Falco Winkler <falcomail95@yahoo.de>
2019-08-12 16:56:08 +0000

    Add tests for neural network loader

--- tests/agents/dql/test_q_network_factory.py
+++ tests/agents/dql/test_q_network_factory.py
@@ -1,6 +1,4 @@
 import json
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D
 import pungi.agents.dql.q_network_factory as factory
 
 
@@ -8,28 +6,25 @@ def test_load_simple_model():
     with open("tests/resources/simple-dqn-architecture.json", "r") as config_file:
         config = json.load(config_file)
         model = factory.make_simple_sequential(config)
-        print(model.summary())
-        # assert model.count_params() == ?
         assert len(model.layers) == 2
         dense1, dense2 = model.layers
         assert dense1.use_bias
         assert dense2.use_bias
         assert dense1.input_shape == (None, 5)
-        # assert dense1.count_params() == ?
+        assert model.count_params() == 22
+        assert dense1.count_params() == 18
 
 
 def test_load_convolutional_model():
     with open("tests/resources/conv-dqn-architecture.json", "r") as config_file:
         config = json.load(config_file)
         model = factory.make_simple_sequential(config)
-        print(model.summary())
-        # assert model.count_params() == ?
         assert len(model.layers) == 3
         conv, flatten, dense = model.layers
         assert conv.use_bias
         assert dense.use_bias
         assert conv.input_shape == (None, 10, 10, 1)
-        # assert conv.output_shape == ?
-        # assert conv.count_params() == ?
-        # assert dense.count_params() == ?
-        # assert model.count_params() == ?
+        assert conv.output_shape == (None, 5, 5, 8)
+        assert conv.count_params() == 4 * 8 + 8
+        assert dense.count_params() == (5 * 5 * 8) * 4 +4
+        assert model.count_params() == conv.count_params() + dense.count_params()
--- tests/mock_policies.py
+++
@@ -1,6 +0,0 @@
-def mock_policy(q_values, episode_number):
-    return episode_number
-
-
-def max_policy(q_values, episode_number):
-    return episode_number
